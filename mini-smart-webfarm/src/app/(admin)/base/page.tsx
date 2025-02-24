2657c318a14dbbf328c819aea25b3b55592765b3
vanthkrab <vanth2548@gmail.com>
2025-02-24 20:25:41 +0700

    weather

--- mini-smart-webfarm/src/app/(admin)/base/page.tsx
+++ mini-smart-webfarm/src/app/(admin)/base/page.tsx
@@ -13,7 +13,10 @@ const BasePage = () => {
             <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)}/>
 
             <main className="fixed top-16 left-0 lg:left-72 right-0 bottom-0 p-6 overflow-auto">
-            {/*    here */}
+                <div className="panel">
+                    <h1 className="text-2xl font-semibold">Dashboard</h1>
+                    <p className="text-gray-500">Welcome to the admin panel</p>
+                </div>
             </main>
         </div>
     );
